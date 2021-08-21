571148cfc75654e7d03460451836dfb7d7ce74c4
DESKTOP-IGKV12I\Administrator <korcstar@gmail.com>
2021-08-22 03:48:47 +0800

    add comment for unused services

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -4,23 +4,23 @@ class MovieDataService {
   getAll(params) {
     return http.get("/movies", { params });
   }
-
+  //not use for now
   get(id) {
     return http.get(`/movies/${id}`);
   }
-
+  //not use for now
   create(data) {
     return http.post("/movies", data);
   }
-
+  //not use for now
   update(id, data) {
     return http.put(`/movies/${id}`, data);
   }
-
+  //not use for now
   delete(id) {
     return http.delete(`/movies/${id}`);
   }
-
+  //not use for now
   deleteAll() {
     return http.delete("/movies");
   }
