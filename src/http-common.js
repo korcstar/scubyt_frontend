0785c8ba04b8fce35d23299d19044d895385a125
DESKTOP-IGKV12I\Administrator <korcstar@gmail.com>
2021-08-24 03:26:19 +0800

    update to use remote api url

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+var base_url = "http://localhost";
+if (typeof window !== "undefined") {
+  base_url = window.location.protocol + "//" + window.location.hostname;
+}
+const baseURL = base_url + ":8080/api";
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: baseURL,
   headers: {
-    "Content-type": "application/json"
-  }
+    "Content-type": "application/json",
+  },
 });
